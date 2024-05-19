4ff6c337ea176100a0a2edf66d728c81f1ab4163
Alejandro Maldonado <am@AM-WEBDEV.local>
2024-05-18 22:34:31 -0500

    change google font

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
-import { Space_Grotesk } from "next/font/google";
+import { Raleway } from "next/font/google";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { Toaster } from "react-hot-toast";
 import { Providers } from "./providers";
 import "./globals.css";
 
-const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
+const raleway = Raleway({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Alejandro M | Web Developer",
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
-      <body className={`${spaceGrotesk.className} bg-gray-300 text-gray-900 pt-28 sm:pt-36`}>
+      <body className={`${raleway.className} bg-gray-300 text-gray-900 pt-28 sm:pt-36`}>
         <Providers>
           <Header />
           {children}
